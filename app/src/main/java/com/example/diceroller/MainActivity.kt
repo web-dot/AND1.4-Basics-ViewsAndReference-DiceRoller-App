9465ed79ff834a467d771ca62446dac8a7b27a0c
sudipto <cg.sudipc@gmail.com>
2023-09-10 01:07:12 +0530

    update ImageView on button click

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -24,9 +25,13 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice objecct with 6 sides and roll it
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        // update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // update the screen with the dice roll(code used before adding ImageView)
+        //val resultTextView: TextView = findViewById(R.id.textView)
+        //resultTextView.text = diceRoll.toString()
+
+        // update screen with the dice image
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 }
 
@@ -37,3 +42,39 @@ class Dice(val numSides: Int) {
         return range.random()
     }
 }
+
+/**
+ * creating a lucky number Dice game in kotlin
+ *
+ *
+ * fun main() {
+ * val dice = Dice(6)
+ * val diceSide = dice.roll()
+ *
+ *if(diceSide == luckyNumber){
+ *    println("You win")
+ *    }
+ *
+ *
+ *    // in place of if, if else, a when state ment can be used, like below
+ *
+ *     when(diceSide){
+ *     luckyNumber -> println("you win")
+ *     1 -> println("you rolled 1, try again")
+ *     2 -> println("you rolled 2, try again")
+ *     3 -> println("you rolled 3, try again")
+ *     6 -> println("you rolled 6, try again")
+ *     5 -> println("you rolled 5, try again")
+}
+ *
+ * }
+ *
+ * class Dice(val numSides: Int){
+ * fun roll(): Int{
+ * val numRange = 1..numSides
+ * val diceRollResult = numRange.random()
+ * return diceRollResult
+ *  }
+ * }
+ *
+ * */
